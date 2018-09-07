be253131cf5f01f2595668a4924928fa328a322c
SummerGift <SummerGift@qq.com>
2018-09-07 16:30:42 +0800

    【完善】：文件系统读写例程

--- readwrite/readwrite_sample.c
+++ readwrite/readwrite_sample.c
@@ -1,127 +1,49 @@
-/* 
- * Copyright (c) 2006-2018, RT-Thread Development Team 
- * 
- * SPDX-License-Identifier: Apache-2.0 
- * 
- * Change Logs: 
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
  * Date             Author      Notes
- * 
+ *
  */
 /*
  * 代码清单：文件读写例子
  *
- * 这个例子演示了如何读写一个文件，特别是写的时候应该如何操作。
+ * 这个例子演示了如何读写一个文件。
  */
 
 #include <rtthread.h>
 #include <dfs_posix.h> /* 当需要使用文件操作时，需要包含这个头文件 */
 
-#define TEST_FN     "/test.dat"
-
-/* 测试用的数据和缓冲 */
-static char test_data[120], buffer[120];
-
 static void readwrite_sample(void)
 {
-    int fd;
-    int index, length;
+    int fd, size;
+    char s[] = "RT-Thread Programmer!", buffer[80];
 
-    /* 只写 & 创建 打开 */
-    fd = open(TEST_FN, O_WRONLY | O_CREAT | O_TRUNC, 0);
-    if (fd < 0)
-    {
-        rt_kprintf("open file for write failed\n");
-        return;
-    }
+    rt_kprintf("Write string %s to test.txt.\n", s);
 
-    /* 准备写入数据 */
-    for (index = 0; index < sizeof(test_data); index ++)
+    /* 以创建和读写模式打开 /text.txt 文件，如果该文件不存在则创建该文件*/
+    fd = open("/text.txt", O_WRONLY | O_CREAT);
+    if (fd >= 0)
     {
-        test_data[index] = index + 48;
-    }
-
-    /* 写入数据 */
-    length = write(fd, test_data, sizeof(test_data));
-    if (length != sizeof(test_data))
-    {
-        rt_kprintf("write data failed\n");
+        write(fd, s, sizeof(s));
         close(fd);
-        return;
     }
 
-    /* 关闭文件 */
-    close(fd);
+    rt_kprintf("Write done.\n");
 
-    /* 只写并在末尾添加打开 */
-    fd = open(TEST_FN, O_WRONLY | O_CREAT | O_APPEND, 0);
-    if (fd < 0)
+    /* 以只读模式打开 /text.txt 文件 */
+    fd = open("/text.txt", O_RDONLY);
+    if (fd >= 0)
     {
-        rt_kprintf("open file for append write failed\n");
-        return;
-    }
-
-    length = write(fd, test_data, sizeof(test_data));
-    if (length != sizeof(test_data))
-    {
-        rt_kprintf("append write data failed\n");
-        close(fd);          
-        return;
-    }
-    /* 关闭文件 */
-    close(fd);
-
-    /* 只读打开进行数据校验 */
-    fd = open(TEST_FN, O_RDONLY, 0);
-    if (fd < 0)
-    {
-        rt_kprintf("check: open file for read failed\n");
-        return;
-    }
-
-    /* 读取数据(应该为第一次写入的数据) */
-    length = read(fd, buffer, sizeof(buffer));
-    if (length != sizeof(buffer))
-    {
-        rt_kprintf("check: read file failed\n");
+        size = read(fd, buffer, sizeof(buffer));
         close(fd);
-        return;
+        if (size < 0)
+            return ;
     }
 
-    /* 检查数据是否正确 */
-    for (index = 0; index < sizeof(test_data); index ++)
-    {
-        if (test_data[index] != buffer[index])
-        {
-            rt_kprintf("check: check data failed at %d\n", index);
-            close(fd);
-            return;
-        }
-    }
-
-    /* 读取数据(应该为第二次写入的数据) */
-    length = read(fd, buffer, sizeof(buffer));
-    if (length != sizeof(buffer))
-    {
-        rt_kprintf("check: read file failed\n");
-        close(fd);
-        return;
-    }
-
-    /* 检查数据是否正确 */
-    for (index = 0; index < sizeof(test_data); index ++)
-    {
-        if (test_data[index] != buffer[index])
-        {
-            rt_kprintf("check: check data failed at %d\n", index);
-            close(fd);
-            return;
-        }
-    }
-
-    /* 检查数据完毕，关闭文件 */
-    close(fd);
-    /* 打印结果 */
-    rt_kprintf("read/write done.\n");
+    rt_kprintf("Read from file test.txt : %s \n", buffer);
 }
 /* 导出到 msh 命令列表中 */
 MSH_CMD_EXPORT(readwrite_sample, readwrite sample);
